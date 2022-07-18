e90e2f507ce8698afb448256983b8e4d399986af
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-07-18 16:47:01 +0530

    Roboshop

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -4,8 +4,8 @@ source components/common.sh
 set -e
 echo "hello I'm front end"
 
-userid = $(id -u)
-if [$userid -ne 0] ;then
+USER_ID = $(id -u)
+if [ $USER_ID -ne 0 ] ; then
     echo "You need to be a root user"
     exit 1
 fi
