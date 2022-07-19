b9a6d4cca327ca8170f4406cf598b0bf88b8b122
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-07-19 11:35:55 +0530

    Mongodb change

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-source components/common.sh
+source . ./common.sh
 COMPONENT=mongod
 LOGFILE="/tmp/$COMPONENTS.log"
 MongodRepo="https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo"
+MongodSchemaRepo="https://github.com/stans-robot-project/mongodb/archive/main.zip"
 sw=nginx
 
 set -e
@@ -14,7 +15,7 @@ stat $?
 
 
 echo -n "Extract sw "
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+curl -s -o /etc/yum.repos.d/mongodb.repo "$MongodRepo"
 stat $?
 
 echo -n "Install software"
@@ -32,17 +33,17 @@ systemctl restart mongod &>> $LOGFILE
 stat $?
 
 echo -n "Download the schema and inject it"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+curl -s -L -o /tmp/mongodb.zip "$MongodSchemaRepo"
 stat $?
 
 echo -n "Extract file to import schema"
 cd /tmp
-unzip mongodb.zip
+unzip mongodb.zip &>> $LOGFILE
 stat $?
 
 echo -n "Run the java script to import mysql schema"
 cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+mongo < catalogue.js &>> $LOGFILE
+mongo < users.js &>> $LOGFILE
 stat $?
 echo -n "Executed Mongodb successfully"
