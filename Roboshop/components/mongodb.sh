93e5ccc38cd2b3c600dd5dbc16cfdd7f3e1a393d
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-07-23 14:13:17 +0530

    Change in bash script for roboshop

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -16,30 +16,30 @@ stat $?
 
 
 echo -n "Extract sw "
-curl -s -o ${mongodTempStore} ${MongodRepo}
+curl -s -o ${mongodTempStore} ${MongodRepo} &>> $LOGFILE
 stat $?
 
 echo -n "Install software"
-yum install -y mongodb-org 
+yum install -y mongodb-org &>> $LOGFILE
 stat $?
 
 echo -n "Update component listening address"
-sed -i  's/127.0.0.1/0.0.0.0/g' /etc/${COMPONENT}.conf 
+sed -i  's/127.0.0.1/0.0.0.0/g' /etc/${COMPONENT}.conf &>> $LOGFILE
 stat $?
 
 echo -n "Enable and restart mongodb"
-systemctl enable ${COMPONENT}
-systemctl restart ${COMPONENT}
+systemctl enable ${COMPONENT} &>> $LOGFILE
+systemctl restart ${COMPONENT} &>> $LOGFILE
 stat $?
 
 
 echo -n "Download the schema and inject it"
-curl -s -L -o /tmp/mongodb.zip "$MongodSchemaRepo"
+curl -s -L -o /tmp/mongodb.zip "$MongodSchemaRepo" &>> $LOGFILE
 stat $?
 
 echo -n "Extract file to import schema"
 cd /tmp
-unzip mongodb.zip &>> $LOGFILE
+unzip mongodb.zip &>> $LOGFILE &>> $LOGFILE
 stat $?
 
 echo -n "Run the java script to import mysql schema"
