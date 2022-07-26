6d3b8f650748d7de20a0c765054787b6589a86b5
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-07-26 11:03:38 +0530

    Change in bash script for roboshop

--- Roboshop/components/payment.sh
+++ Roboshop/components/payment.sh
@@ -9,25 +9,25 @@ set -e
 echo -e "\e[32m hello I'm Payment \e[0m"
 
 echo -n "Check for root user "
-checkUser
+checkUser &>> $LOGFILE
 stat $?
 
 echo -n "Install python"
-yum install python36 gcc python3-devel -y
+yum install python36 gcc python3-devel -y &>> $LOGFILE
 stat $?
 
 echo -n "Switch to roboshop ${COMPONENT} and run "
-id $APPUSER &>> $LOGFILE || useradd $APPUSER
+id $APPUSER &>> $LOGFILE || useradd $APPUSER &>> $LOGFILE
 stat $? 
 
 echo -n "Download Repo"
 cd /home/roboshop
-curl -L -s -o /tmp/payment.zip "https://github.com/stans-robot-project/payment/archive/main.zip"
-unzip -o /tmp/payment.zip
-mv payment-main payment
+curl -L -s -o /tmp/payment.zip "https://github.com/stans-robot-project/payment/archive/main.zip" &>> $LOGFILE
+unzip -o /tmp/payment.zip &>> $LOGFILE
+mv payment-main payment &>> $LOGFILE
 stat $?
 
 echo -n "Install dependancies"
 cd /home/roboshop/payment 
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &>> $LOGFILE
 stat $?
