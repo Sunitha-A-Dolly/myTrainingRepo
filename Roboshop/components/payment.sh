1d05207ce7a3742a9e4b80482bdf2c3172720070
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-07-26 11:28:07 +0530

    Change in bash script for roboshop

--- Roboshop/components/payment.sh
+++ Roboshop/components/payment.sh
@@ -36,9 +36,4 @@ cd /home/roboshop/payment
 pip3 install -r requirements.txt 
 stat $?
 
-echo -n "Update payment.ini file"
-uidValue = ${id -u}
-gidValue = ${id -g}
-echo $uidValue $gidValue
-sed -i -e 's/uid = 1/$uidValue/g' /home/$APPUSER/${COMPONENT}/payment.ini  -e 's/gid = 1/$gidValue/g' /home/$APPUSER/${COMPONENT}/payment.ini
-stat $?
+
