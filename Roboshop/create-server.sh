c7b0e76c1b46f7aae5d3e8a684ee3551aa27ddda
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-08-16 12:20:41 +0530

    Change in bash script for roboshop

--- Roboshop/create-server.sh
+++ Roboshop/create-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/\"//g')
 echo $AMI_ID
+Component=$1
+aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${Component}}]" --security-group-ids sg-02d54b5554e44973b 
