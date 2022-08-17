dd0628dfd6f9f642343bc0ac71610910ea4229ef
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-08-17 09:37:52 +0530

    Change in bash script for roboshop

--- Roboshop/create-server.sh
+++ Roboshop/create-server.sh
@@ -8,14 +8,36 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImag
 echo $AMI_ID
 
 # Get Component as Argument
+if [ $1 == null ] ; then
+    echo -n "Enter valid argument all/Component name"
+    exit 1
+fi
 Component=$1
+SecurityGroupId='sg-02d54b5554e44973b'
+HostedZoneId='Z03158041QD430SCB65LE'
 
-# Get Private IP
-PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]" --security-group-ids sg-02d54b5554e44973b | jq '.Instances[].PrivateIpAddress' | sed -e 's/\"//g')
-echo $PRIVATE_IP
 
-sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/$Component/" route53.json > /tmp/record.json
 
-# Create route53 record
-aws route53 change-resource-record-sets --hosted-zone-id Z03158041QD430SCB65LE --change-batch file:///tmp/record.json | jq
+create_server() {
+
+        echo -n " $Component component creation in progress "
+        # Get Private IP
+        PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]" --security-group-ids $SecurityGroupId | jq '.Instances[].PrivateIpAddress' | sed -e 's/\"//g')
+        echo $PRIVATE_IP
+
+        # replace ipaddress and component name
+        sed -e "s/IPADDRESS/$PRIVATE_IP/" -e "s/COMPONENT/$Component/" route53.json > /tmp/record.json
+
+        # Create route53 record
+        aws route53 change-resource-record-sets --hosted-zone-id $HostedZoneId --change-batch file:///tmp/record.json | jq
+
+}
+if [ $1 == all ] ; then
+    for component in frontend catalogue cart user shipping payment mysql rabbitmq redis mongodb dispatch ; do
+      Component=$component
+      create_server
+    done
+else
+    create_server
+fi
 
