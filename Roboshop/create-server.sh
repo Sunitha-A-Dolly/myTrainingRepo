0d05787ad18d3af379cb4bd8c5a9a9056900aa77
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-08-17 08:45:48 +0530

    Change in bash script for roboshop

--- Roboshop/create-server.sh
+++ Roboshop/create-server.sh
@@ -11,7 +11,7 @@ echo $AMI_ID
 Component=$1
 
 # Get Private IP
-PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]" --security-group-ids sg-02d54b5554e44973b | jq '.Instances[].PrivateIpAddress' | sed -e 's/\"//g')
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]" --security-group-ids sg-02d54b5554e44973b | jq '.Instances[].PrivateIpAddress' | sed -e 's/*//g')
 echo $PRIVATE_IP
 
 sed -e "/s/IPADDRESS/$PRIVATE_IP/" -e "/s/COMPONENT/$Component/" route53.json > /tmp/record.json
