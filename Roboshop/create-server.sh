d1cc6c81c537e27bd08381ece2d7bfa0a5f0d813
Sunitha Jayapaul <sunselfinsight.2022@gmail.com>
2022-08-17 08:43:14 +0530

    Change in bash script for roboshop

--- Roboshop/create-server.sh
+++ Roboshop/create-server.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+
+# Create server
+# Route53 to create record and configure dns
+
+# Get AMI Id
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/\"//g')
 echo $AMI_ID
+
+# Get Component as Argument
 Component=$1
-aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]" --security-group-ids sg-02d54b5554e44973b | jq
+
+# Get Private IP
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]" --security-group-ids sg-02d54b5554e44973b | jq '.Instances[].PrivateIpAddress' | sed -e 's/\"//g')
+echo $PRIVATE_IP
+
+sed -e "/s/IPADDRESS/$PRIVATE_IP/" -e "/s/COMPONENT/$Component/" route53.json > /tmp/record.json
+
+# Create route53 record
+aws route53 change-resource-record-sets --hosted-zone-id Z03158041QD430SCB65LE --change-batch file:///tmp/record.json | jq
+
